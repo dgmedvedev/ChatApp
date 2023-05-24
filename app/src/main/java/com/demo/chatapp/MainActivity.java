f0d45e8ced4779069641f41916b005cc84c4a5eb
dgmedvedev <dg-medvedev@mail.ru>
2023-05-24 17:52:52 +0300

    Достучался до Галереи, добавлен Swipe

--- app/src/main/java/com/demo/chatapp/MainActivity.java
+++ app/src/main/java/com/demo/chatapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.demo.chatapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,18 +20,36 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.demo.chatapp.adapters.MessagesAdapter;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult);
+
+    private final ActivityResultLauncher<Intent> getImageLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            this::getImageResult);
+
+    private final String COLLECTION_NAME = "messages";
 
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
@@ -35,8 +58,25 @@ public class MainActivity extends AppCompatActivity {
     private MessagesAdapter adapter;
     private EditText editTextMessage;
     private ImageView imageViewSendMessage;
+    private ImageView imageViewAddImage;
 
     private String author;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        swipe();
+        db.collection(COLLECTION_NAME).orderBy("date").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (value != null) {
+                    List<Message> messages = value.toObjects(Message.class);
+                    adapter.setMessages(messages);
+                    recyclerViewMessages.scrollToPosition(adapter.getItemCount() - 1);
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,25 +102,11 @@ public class MainActivity extends AppCompatActivity {
         recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
         editTextMessage = findViewById(R.id.editTextMessage);
         imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+        imageViewAddImage = findViewById(R.id.imageViewAddImage);
         adapter = new MessagesAdapter();
         recyclerViewMessages.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewMessages.setAdapter(adapter);
         author = "Дмитрий";
-
-        imageViewSendMessage.setOnClickListener(view -> {
-            sendMessage();
-        });
-
-        db.collection("messages").orderBy("date").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if (value != null) {
-                    List<Message> messages = value.toObjects(Message.class);
-                    adapter.setMessages(messages);
-                    recyclerViewMessages.scrollToPosition(adapter.getItemCount() - 1);
-                }
-            }
-        });
 
         if (mAuth.getCurrentUser() != null) {
             Toast.makeText(this, "Logged", Toast.LENGTH_SHORT).show();
@@ -88,13 +114,22 @@ public class MainActivity extends AppCompatActivity {
             signOut();
         }
 
+        imageViewSendMessage.setOnClickListener(view -> {
+            sendMessage();
+        });
 
+        imageViewAddImage.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT); // получаем контент
+            intent.setType("image/jpeg"); // указываем какой именно контент необходимо получить
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true); // контент только с локального хранилища
+            getImageLauncher.launch(intent);
+        });
     }
 
     private void sendMessage() {
         String textOfMessage = editTextMessage.getText().toString().trim();
         if (!textOfMessage.isEmpty()) {
-            db.collection("messages").add(new Message(author, textOfMessage, System.currentTimeMillis()))
+            db.collection(COLLECTION_NAME).add(new Message(author, textOfMessage, System.currentTimeMillis()))
                     .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                         @Override
                         public void onSuccess(DocumentReference documentReference) {
@@ -111,7 +146,84 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void signOut() {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.GoogleBuilder().build());
+                        Intent signInIntent = AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build();
+                        signInLauncher.launch(signInIntent);
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (user != null) {
+                Toast.makeText(MainActivity.this, user.getEmail(), Toast.LENGTH_SHORT).show();
+                author = user.getEmail();
+            }
+        } else {
+            if (response != null) {
+                Toast.makeText(MainActivity.this, "Error: " + response.getError(), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void getImageResult(ActivityResult result) {
+        if (result.getResultCode() == RESULT_OK) {
+            Intent data = result.getData();
+            if (data != null) {
+                Uri uri = data.getData();
+                Toast.makeText(MainActivity.this, uri.toString(), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "Error getImageResult", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void swipe() {
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView,
+                                  @NonNull RecyclerView.ViewHolder viewHolder,
+                                  @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                db.collection(COLLECTION_NAME).orderBy("date").get()
+                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot querySnapshots) {
+                                for (int i = 0; i < querySnapshots.getDocuments().size(); i++) {
+                                    if (i == viewHolder.getAdapterPosition()) {
+                                        String messageId = querySnapshots.getDocuments().get(i).getId();
+                                        db.collection(COLLECTION_NAME).document(messageId).delete();
+                                        return;
+                                    }
+                                }
+                            }
+                        });
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerViewMessages);
     }
 }
